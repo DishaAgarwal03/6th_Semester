8e3c3d5eb9d80e000481406a96f3f40a59b302f2
Disha Agarwal <97750861+DishaAgarwal03@users.noreply.github.com>
2024-03-11 22:30:24 +0530

    Update views.py

--- Internet_Technologies_Lab/WEEK7_DJANGO_FORMS/Q2/views.py
+++ Internet_Technologies_Lab/WEEK7_DJANGO_FORMS/Q2/views.py
@@ -1,28 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import SubForm
+#from .forms import DisplayForm
 
-# Create your views here.
+def first_page(request):
+    if request.method == 'POST':
+        form = SubForm(request.POST)
+        if form.is_valid():
+            request.session['name'] = form.cleaned_data['name']
+            request.session['roll'] = form.cleaned_data['roll']
+            request.session['subject'] = form.cleaned_data['subject']
+            return redirect('second_page')
+    else:
+        form = SubForm()
+    return render(request, 'first_page.html', {'form': form})
 
-def home(request):
-    context = {}
-    form = SubForm(request.POST or None)
-    context['form'] = form
-    return render(request,'home.html',context)
-
-def display(request):
-    context = {}
-    subject = 'Mathematics'
-    name = 'Disha'
-    roll = '16'
-    if request.method == "POST":
-        loginform  = SubForm(request.POST)
-        if loginform.is_valid():
-            subject = loginform.cleaned_data['subject']
-            name = loginform.cleaned_data['name']
-            roll = loginform.cleaned_data['roll']
-        else:
-            loginform  = SubForm()
-    context['subject'] = subject
-    context['name'] = name
-    context['roll'] = roll
-    return render(request,'display.html',context)
+def second_page(request):
+    if request.method == 'POST':
+        return redirect('first_page')
+    else:
+        #form = DisplayForm(initial={'label_text': f"Name: {request.session.get('name')} | Roll: {request.session.get('roll')} | Subject: {request.session.get('subject')}"})
+        name = request.session.get('name')
+        roll = request.session.get('roll')
+        subject = request.session.get('subject')
+        return render(request, 'second_page.html', {'name': name, 'roll': roll, 'subject':subject})
+    #return render(request, 'second_page.html', {'form': form})
